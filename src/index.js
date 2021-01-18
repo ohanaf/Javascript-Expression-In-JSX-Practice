e4ce4fd9ec739773053a4b06db069038a13aeae2
Hanaf Othmane <ohanaf@users.noreply.github.com>
2021-01-18 06:09:57 -0500

    Add-paragraphs added two paragraphs with my name and current year

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-//Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
+const MyName = "Othmane Hanaf";
+var CurrentYear = new Date().getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {MyName}.</p>
+    <p>Copyright {CurrentYear}.</p>
+  </div>,
+  document.getElementById("root")
+);
+
 //It should display 2 paragraph HTML elements.
 //The paragraphs should say:
 //Created by YOURNAME.
